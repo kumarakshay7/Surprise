4b8b50b54921d03b2272b030127fb31e48ab9703
Akshay Kumar <142158254+kumarakshay7@users.noreply.github.com>
2025-02-27 14:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if name:
     
     score = sum([1 for key in correct_answers if user_answers.get(key) == correct_answers[key]])
     
-    if score >= 6:
+    if score >= 7:
         if name.strip().lower() == "ahona ayan":
             st.success("Congratulations, Ahona Ayan! You got most answers correct! 🎉")
             time.sleep(1)
